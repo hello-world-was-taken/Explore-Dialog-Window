e9204e28b277522ee484e292baf1ca411ba196f5
hello_world <haileformal@gmail.com>
2021-10-05 10:50:17 +0300

    add button to pop-up alert box

--- app/src/main/java/com/example/exploredialogwindow/MainActivity.kt
+++ app/src/main/java/com/example/exploredialogwindow/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.exploredialogwindow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // button for opening the dialog window
+        button.setOnClickListener { v ->
+            val alertBox = myDialog()
+
+            alertBox.show(supportFragmentManager, "123")
+        }
     }
 }
