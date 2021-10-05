cd283f757da348b455a522d7df4f5c58fbbbea16
hello_world <haileformal@gmail.com>
2021-10-05 10:38:30 +0300

    code the myDialog class

--- app/src/main/java/com/example/exploredialogwindow/myDialog.kt
+++ app/src/main/java/com/example/exploredialogwindow/myDialog.kt
@@ -1,7 +1,22 @@
 package com.example.exploredialogwindow
 
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
 class myDialog : DialogFragment() {
-    
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // using builder, since this dialog has a simple UI
+        val builder = AlertDialog.Builder(this.requireActivity())
+
+        builder.setMessage("This is an alert box").setPositiveButton("OK", { dialog, id ->
+            // TODO: FOR LATER
+        }
+        ).setNegativeButton("CANCLE", {dialog, id ->
+            // TODO: FOR LATER
+        })
+
+        return builder.create()
+    }
 }
